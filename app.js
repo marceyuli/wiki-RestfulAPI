424357600acdc33a64eea2ffa11e972ac483fb3e
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-01-24 16:45:48 -0500

    Delete one

--- app.js
+++ app.js
@@ -96,7 +96,21 @@ app.route("/articles")
                 }
                } 
             )
-        });
+        })
+
+        .delete(function(req,res){
+            Article.deleteOne(
+                {title: req.params.articleTitle},
+                function(err){
+                    if(!err){
+                        res.send("Succes!");
+                    }
+                    else{
+                        res.send(err);
+                    }
+                }
+            )
+        })
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
